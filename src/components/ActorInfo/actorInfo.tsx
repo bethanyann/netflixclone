c985d0ea3177afbcdfe5607e9134765c0dd2a500
bethanyann <bethanyann44@gmail.com>
2022-06-29 12:45:37 -0500

    renaming components; adding actor info component

---
+++ src/components/ActorInfo/actorInfo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FormProvider } from 'react-hook-form';
+//styles
+import { Wrapper, Content, Text } from './ActorInfo.styles';
+//config
+import { ACTOR_BIO_URL, API_KEY, LANGUAGE } from '../../config';
+
+
+const ActorInfo = () => {
+    //use params here to get the actor id out of the url
+    //convert it to number
+
+    //get actor data from the hook here
+
+    //create the hook
+    //create the route param
+    //create the actor info component
+    //create the styles
+
+}
