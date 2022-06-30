a28a8dca206477cfd16eeda92cbf8544e929168a
bethanyann <bethanyann44@gmail.com>
2022-06-30 00:03:03 -0500

    actor component & custom scrollbar

--- src/components/ActorInfo/ActorInfo.styles.ts
+++ src/components/ActorInfo/ActorInfo.styles.ts
@@ -1,7 +1,86 @@
 import styled from 'styled-components';
+//import image sizes
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../../config';
 
-export const Wrapper = styled.div``;
 
-export const Content = styled.div``;
+export const Wrapper = styled.div`
+    background: var(--darkGray);
+    background-size: cover;
+    background-position: center;
+    padding: 40px 20px;
+    animation: animateMovieInfo 1s;
 
-export const Text = styled.div``;
+    @keyframes animateMovieInfo {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 20px;
+
+    @media screen and (max-width: 768px) {
+        display: block; 
+        max-height: none;
+    }
+`;
+
+export const Text = styled.div`
+    width: 100%;
+    padding: 20px 40px;
+    color: var(--white);
+    overflow: hidden;
+
+    .actor-info {
+        display: flex;
+        justify-content: flex-start;
+        margin-top: 20px;
+    }
+
+    .score {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      //  width: 40px;
+      //  height: 40px;
+        color: var(--white);
+        font-weight: 800;
+        margin: 0;
+    }
+
+    .director {
+        margin: 0 0 0 40px;
+
+        p {
+            margin: 0;
+        }
+    }
+
+    h1 {
+        display: flex;
+        justify-content: space-between;
+        @media screen and (max-width: 768px) {
+            font-size: var(--fontBig);
+        }
+    }
+
+    .actor-bio {
+        white-space: pre-wrap;
+       
+    }
+
+    img {
+        width: 55px;
+        height: 30px;
+        display: flex;
+    }
+`;
+
